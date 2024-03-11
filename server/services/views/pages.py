0d78ec0a44c33fb2fdcadb0738d49576152dacc3
bRuttaZz <agrajpdasprivate@gmail.com>
2024-03-11 10:24:54 +0530

    update in book price showing in transact page

--- server/services/views/pages.py
+++ server/services/views/pages.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template
 
-from ...settings import URL_PREFIX
+from ...settings import URL_PREFIX, SINGLE_BOOK_RENT
 
 views = Blueprint('pages', __name__)
 
 @views.route("/")
 def main_page():
-    return render_template('main.html', URL_PREFIX=URL_PREFIX )
+    return render_template('main.html', URL_PREFIX=URL_PREFIX, transact_fee=SINGLE_BOOK_RENT )
 
 @views.route("/login")
 def login_page():
